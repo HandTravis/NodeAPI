b9cc1cda55579a0b32a92ffff77157dbcac6a565
handtravis <travishand02@gmail.com>
2024-07-08 16:06:17 -0700

    added functionality to get and update an item in the db by id

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const Product = require ('./models/productModel')
 const app = express()
 
 app.use(express.json())
-
+app.use(express.urlencoded({extended: false}))
 //routes
 
 app.get('/', (req, res) => {
@@ -15,7 +15,26 @@ app.get('/blog', (req, res) => {
     res.send('Hello blog, my name is Travis')
 })
 
-app.post('/product', async(req, res) => {
+app.get('/products', async(req, res)=> {
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id)
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/products', async(req, res) => {
     try {
         const product = await Product.create(req.body)
         res.status(200).json(product)
@@ -25,6 +44,19 @@ app.post('/product', async(req, res) => {
     }
 })
 
+app.put('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const product = await Product.findByIdAndUpdate(id, req.body)
+        if (!product){
+            return res.status(404).json({message: 'cannot find any product with ID ${id}'})
+        }
+        const updatedProduct = await Product.findById(id)
+        res.status(200).json(updatedProduct)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
 mongoose.connect('mongodb+srv://admin:2016474@travisapi.mmzwmwi.mongodb.net/Node-API?retryWrites=true&w=majority&appName=TravisAPI')
     .then(() => {
