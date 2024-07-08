bc28d8a00c0985b9de05f91f5c8f71030d5f35a3
handtravis <travishand02@gmail.com>
2024-07-08 15:10:39 -0700

    added nodemon for easy development, and product model for json object submission

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Product = require ('./models/productModel')
 const app = express()
+
+app.use(express.json())
 
 //routes
 
@@ -7,6 +11,26 @@ app.get('/', (req, res) => {
     res.send('Hello NODE API')
 })
 
-app.listen(3000, ()=> {
-    console.log('Node API app is running on port 3000')
+app.get('/blog', (req, res) => {
+    res.send('Hello blog my name is Travis')
+})
+
+app.post('/product', async(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+mongoose.
+connect('mongodb+srv://admin:2016474@travisapi.mmzwmwi.mongodb.net/Node-API?retryWrites=true&w=majority&appName=TravisAPI')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, ()=> {
+        console.log('Node API app is running on port 3000')
+    })
+
+}).catch((error) => {
+    console.log(error)
 })
