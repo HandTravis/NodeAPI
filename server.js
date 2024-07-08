d5a9d27e6c60f37926cd545de4114c6b246dd19a
handtravis <travishand02@gmail.com>
2024-07-08 16:10:33 -0700

    added db delete functionality

--- server.js
+++ server.js
@@ -58,6 +58,19 @@ app.put('/products/:id', async(req, res) => {
     }
 })
 
+app.delete('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const product = await Product.findByIdAndDelete(id)
+        if (!product) {
+            return res.status(404).json({message: 'cannot find any product with ID ${id}'})
+        }
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 mongoose.connect('mongodb+srv://admin:2016474@travisapi.mmzwmwi.mongodb.net/Node-API?retryWrites=true&w=majority&appName=TravisAPI')
     .then(() => {
         console.log('connected to MongoDB')
